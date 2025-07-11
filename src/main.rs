024c151508aa4df070b0d0d29b1ebefd19bc19fb
hgrsd <daniel@hgrsd.nl>
2025-07-11 23:34:23 +0100

    loop

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::io::{self, BufRead, Write};
+use std::io::{self, Write};
 
-use crate::provider::model::{Message, Model};
+use crate::provider::{
+    anthropic::anthropic_model::AnthropicModel,
+    model::{Message, Model},
+};
 
 mod provider;
 mod session;
@@ -9,9 +12,11 @@ mod tool;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let anthropic_key = std::env::var("ANTHROPIC_API_KEY").expect("ANTHROPIC_API_KEY must be set");
-    let client = provider::anthropic::anthropic_model::AnthropicModel::new(anthropic_key, "claude-sonnet-4-20250514".to_owned(), 10_000, Some("You are an agentic code assistant called deputy. You will refer to yourself as the user's deputy. Use the tools available and your reasoning power to assist the user as best as you can.".to_owned()), None);
+    let client = AnthropicModel::new(anthropic_key, "claude-sonnet-4-20250514".to_owned(), 10_000, Some("You are an agentic code assistant called deputy. You will refer to yourself as the user's deputy. Use the tools available and your reasoning power to assist the user as best as you can.".to_owned()), None);
     let mut message_history = Vec::new();
     loop {
+        print!("> ");
+        io::stdout().flush()?;
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
@@ -21,7 +26,14 @@ async fn main() -> anyhow::Result<()> {
             .send_message(message.clone(), message_history.clone())
             .await?;
         message_history.push(message);
-        println!("{:?}", response);
+        for response_chunk in &response {
+            match response_chunk {
+                Message::Model(text) => {
+                    println!("{}", text);
+                }
+                _ => unimplemented!(),
+            }
+        }
         message_history.extend(response);
     }
 }
